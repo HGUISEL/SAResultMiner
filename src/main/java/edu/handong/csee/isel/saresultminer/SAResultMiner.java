cf936ac9566fe9f0f0c3d5ab8a6e3705ecdb5eae
YoonhoChoi96 <uenho830@gmail.com>
2020-07-25 16:07:29 +0900

    change rule

--- src/main/java/edu/handong/csee/isel/saresultminer/SAResultMiner.java
+++ src/main/java/edu/handong/csee/isel/saresultminer/SAResultMiner.java
@@ -39,7 +39,7 @@ public class SAResultMiner {
 		//@param pmd command location
 		PMD pmd = new PMD("./pmd-bin-6.25.0/bin/run.sh");
 		String pmdVersion = "6.25";
-		String rule = "category/java/errorprone.xml/InvalidLogMessageFormat";
+		String rule = "category/java/errorprone.xml/MissingStaticMethodInNonInstantiatableClass";
 		ArrayList<Alarm> alarms = new ArrayList<>();
 		
 		//utils instances
