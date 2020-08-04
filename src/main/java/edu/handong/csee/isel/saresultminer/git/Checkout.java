60f2dfc8f034f0763d7aa95d3fb6d96e86fd7068
YoonhoChoi96 <uenho830@gmail.com>
2020-08-05 03:21:47 +0900

    set forced in Jgit

--- src/main/java/edu/handong/csee/isel/saresultminer/git/Checkout.java
+++ src/main/java/edu/handong/csee/isel/saresultminer/git/Checkout.java
@@ -1,12 +1,5 @@
 package edu.handong.csee.isel.saresultminer.git;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.apache.commons.exec.CommandLine;
-import org.apache.commons.exec.DefaultExecutor;
-import org.apache.commons.exec.ExecuteException;
-import org.apache.commons.exec.ExecuteWatchdog;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.CheckoutConflictException;
 import org.eclipse.jgit.api.errors.GitAPIException;
@@ -18,36 +11,19 @@ public class Checkout {
 	public void checkout(Git git, String commitID, int cnt) {
 		System.out.println("INFO: Checkout Start");
 		long start = System.currentTimeMillis();
-//		try {
-		try {				
-			CommandLine cmdLine = new CommandLine("git");
-			cmdLine.addArgument("checkout");
-			cmdLine.addArgument("-f");
-			cmdLine.addArgument(commitID);
-			cmdLine.addArgument("--quiet");
-			DefaultExecutor executor = new DefaultExecutor();
-			int[] exitValues = {0};
-			executor.setExitValues(exitValues);
-			ExecuteWatchdog watchdog = new ExecuteWatchdog(60000);
-			executor.setWatchdog(watchdog);		
-			int exitValue = executor.execute(cmdLine);		
-			} catch (ExecuteException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-//			git.checkout().setForceRefUpdate(true).setForced(true).setName(commitID).call();
-//		} catch (RefAlreadyExistsException e) {
-//			e.printStackTrace();
-//		} catch (RefNotFoundException e) {
-//			e.printStackTrace();
-//		} catch (InvalidRefNameException e) {
-//			e.printStackTrace();
-//		} catch (CheckoutConflictException e) {
-//			e.printStackTrace();
-//		} catch (GitAPIException e) {
-//			e.printStackTrace();
-//		}
+		try {			
+			git.checkout().setForced(true).setName(commitID).call();
+		} catch (RefAlreadyExistsException e) {
+			e.printStackTrace();
+		} catch (RefNotFoundException e) {
+			e.printStackTrace();
+		} catch (InvalidRefNameException e) {
+			e.printStackTrace();
+		} catch (CheckoutConflictException e) {
+			e.printStackTrace();
+		} catch (GitAPIException e) {
+			e.printStackTrace();
+		}
 		long end = System.currentTimeMillis();
 		if(cnt == 0) {
 			System.out.println("INFO: Checkout to Initial Commit Finished" + "(" + (end-start)/1000 + " sec.)" + " Commit ID: " + commitID);
